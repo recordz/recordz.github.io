92ec9ec0ad033b489694b1d2a859116388d8a660
Nishil Shah <nishil.shah17@gmail.com>
2015-05-30 11:09:06 -0400

    bg

--- js/mobile.js
+++ js/mobile.js
@@ -1,3 +1,6 @@
+var vidContainer = document.getElementById('vidContainer').style.height;
+document.getElementById('banner').style.height = vidContainer;
+
 var isMobile = false;
 
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
