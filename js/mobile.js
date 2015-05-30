b3a9fd4b4cb559e4c636baeeb66a68f349e4ba6b
Nishil Shah <nishil.shah17@gmail.com>
2015-05-30 11:57:54 -0400

    fixed video resizing

--- js/mobile.js
+++ js/mobile.js
@@ -1,6 +1,3 @@
-var vidContainer = document.getElementById('vidContainer').style.height;
-document.getElementById('banner').style.height = vidContainer;
-
 var isMobile = false;
 
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
