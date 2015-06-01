453182a975ddade2f93bfa9bb4b42c593873bdd2
Nishil Shah <nishil.shah17@gmail.com>
2015-06-01 12:27:42 -0400

    major update experiencers

--- js/mobile.js
+++ js/mobile.js
@@ -6,6 +6,7 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 
 if(isMobile) {
   var banner = document.getElementById('banner');
-  banner.className = "banner-mobile";
-  banner.innerHTML = '';
+  banner.style.backgroundImage = "url('images/bg-mobile.jpg')";
+  banner.innerHTML = '<div style="padding-top:20%" align="center"><h1 style="font-family:Copperplate,\'Copperplate Gothic Light\', fantasy;font-size:80px;color:#fff">RECORD<font color="red">Z</font></h1><a class="download" style="font-family: Copperplate, \'Copperplate Gothic Light\', fantasy;" href="#">COMING SOON</a></div>';
+	
 }
